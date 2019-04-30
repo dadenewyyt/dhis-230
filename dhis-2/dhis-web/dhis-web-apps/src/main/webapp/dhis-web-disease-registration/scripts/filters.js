90dbaafc88b03cdfb6f9e45e77180e1402477d2f
abyot <abyota@gmail.com>
2019-04-30 11:43:03 +0200

    Filter out existing disease from new entry

--- dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-disease-registration/scripts/filters.js
+++ dhis-2/dhis-web/dhis-web-apps/src/main/webapp/dhis-web-disease-registration/scripts/filters.js
@@ -2,4 +2,28 @@
 
 /* Filters */
 
-var diseaseRegistrationFilters = angular.module('diseaseRegistrationFilters', []);
+var diseaseRegistrationFilters = angular.module('diseaseRegistrationFilters', [])
+
+.filter('existingDisease', function(){
+    
+    return function( items, existingItems ){
+        
+    	if( !items ){
+            return [];
+    	}
+    	
+    	if( !existingItems ){
+            return items;
+    	}
+    	
+        var filteredItems = [], existingItemIds = $.map(existingItems, function(item){return item.id;});
+            
+        angular.forEach(items, function(item){                
+            if( existingItemIds.indexOf(item.id) === -1 ){
+                filteredItems.push(item);
+            }
+        });
+        
+        return filteredItems;
+    };
+});
